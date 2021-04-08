10ae796b4e2fadf89071ae06b6b5a69647462cc2
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-04-08 23:01:10 +0300

    making a timer
    days and hours are not working correctly

---
+++ index.js
@@ -0,0 +1,57 @@
+class CountdownTimer {
+  constructor(selector, targetDate = 0) {
+    this.element = document.querySelector(selector);
+    this.targetDate = targetDate;
+  }
+
+  getSeconds() {
+    const sec = Math.floor((this.targetDate % (1000 * 60)) / 1000);
+    return transformValues(sec);
+  }
+
+  getMinutes() {
+    const min = Math.floor((this.targetDate % (1000 * 60 * 60)) / (1000 * 60));
+    return transformValues(min);
+  }
+
+  getHours() {
+    const hours = Math.floor(
+      (this.targetDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    return transformValues(hours);
+  }
+
+  getDays() {
+    const days = Math.floor(this.targetDate / (1000 * 60 * 60 * 24));
+    return transformValues(days);
+  }
+
+  render() {
+    this.element.innerHTML = `
+    <span class="value" data-value="days">${this.getDays()}</span> : 
+    <span class="value" data-value="hours">${this.getHours()}</span> : 
+    <span class="value" data-value="mins">${this.getMinutes()}</span> : 
+    <span class="value" data-value="secs">${this.getSeconds()}</span>
+    `;
+  }
+
+  init() {
+    const interval = setInterval(() => {
+      this.targetDate -= 1000;
+      if (this.targetDate <= 0) {
+        this.targetDate = 0;
+        clearInterval(interval);
+      }
+
+      this.render();
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer("#timer-1", new Date("Jul 17, 2021"));
+
+timer.init();
+
+function transformValues(value) {
+  return String(value).padStart(2, "0");
+}
